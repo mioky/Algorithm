9f3ae78ee550270d11801e64acab6d34da9667c3
mioky <57795624+mioky@users.noreply.github.com>
2020-09-22 09:32:26 +0900

    2020.09.21

---
+++ BOJ/연산자끼워넣기.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+
+int N;
+int A[11]; //수열
+int op[4]; //연산자
+int _min = 1000000000;
+int _max = -1000000000;
+
+void DFS(int cnt, int total) { //DFS 인자는 인자내에서만 연산하고 그외에는 연산x
+	if (cnt == N - 1) { //연산 종료
+		if (_min > total) _min = total;
+		if (_max < total) _max = total;
+		return;
+	}
+	for (int i = 0; i < 4; i++) { //덧 , 뺄, 곱, 나
+		if (op[i] != 0) { //연산이 남아 있다면
+			op[i]--;
+			if (i == 0) //덧
+				DFS(cnt + 1, total+ A[cnt + 1]);
+			else if(i == 1) //뺄
+				DFS(cnt + 1, total - A[cnt + 1]);
+			else if(i == 2) //곱
+				DFS(cnt + 1, total * A[cnt + 1]);
+			else //나
+				DFS(cnt + 1, total / A[cnt + 1]);
+			op[i]++;
+		}
+	}
+}
+
+int main() {
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+		scanf("%d", &A[i]);
+	for (int i = 0; i < 4; i++)
+		scanf("%d", &op[i]);
+
+	DFS(0, A[0]);
+
+	printf("%d\n%d", _max, _min);
+	return 0;
+}
